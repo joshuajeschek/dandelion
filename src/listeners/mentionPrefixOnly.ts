c94484a6432ca7f2b25f5ae620542ad74a948e1a
joshuajeschek <joshua.jeschek@gmail.com>
2022-03-14 20:53:17 +0100

    fix(listeners): :bug: mentionPrefixOnly
    added database access

--- src/listeners/mentionPrefixOnly.ts
+++ src/listeners/mentionPrefixOnly.ts
@@ -4,7 +4,11 @@ import type { Message } from 'discord.js';
 
 export class UserEvent extends Listener<typeof Events.MentionPrefixOnly> {
 	public async run(message: Message) {
-		const prefix = this.container.client.options.defaultPrefix;
+		const res = await this.container.db.guild.findFirst({
+			where: { id: message.guildId ?? undefined },
+			select: { prefix: true }
+		});
+		const prefix = res ? res.prefix : this.container.client.options.defaultPrefix;
 		return message.channel.send(prefix ? `My prefix in this guild is: \`${prefix}\`` : 'You do not need a prefix in DMs.');
 	}
 }
